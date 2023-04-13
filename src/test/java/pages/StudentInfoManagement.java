08f21b4d6edc2fb3a130f545d4afff501f775ce0
unknown <selcuk0675@gmail.com>
2023-04-13 23:54:04 +0300

    v21

---
+++ src/test/java/pages/StudentInfoManagement.java
@@ -0,0 +1,84 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class StudentInfoManagement {
+    public StudentInfoManagement() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "(//h5)[1]")
+    public WebElement studentInfoText;
+    @FindBy(xpath = "//*[@id='lessonId']")
+    public WebElement chooseLessonTextBox;
+
+    @FindBy(xpath = "//*[@id='studentId']")
+    public WebElement chooseStudentTextBox;
+
+    @FindBy(xpath = "//*[@id='educationTermId']")
+    public WebElement chooseEducationTermTextBox;
+
+    @FindBy(xpath = "//*[@id='absentee']")
+    public WebElement absenteeTextBox;
+
+    @FindBy(xpath = "//*[@id='midtermExam']")
+    public WebElement midtermExamTextBox;
+
+
+    @FindBy(xpath = "//*[@id='finalExam']")
+    public WebElement finalExamTextBox;
+
+    @FindBy(xpath = "//*[@id='infoNote']")
+    public WebElement infoNoteTextBox;
+
+    @FindBy(xpath = " (//*[@type='button'])[5]")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "(//h5)[2]")
+    public WebElement studentInfoListText;
+
+
+    @FindBy(xpath = "(//*[@class='btn btn-danger'])[1]")
+    public WebElement deleteButtonInStudentInfo;
+    @FindBy(xpath = "(//*[@class='fa-solid fa-pencil'])[1]")
+    public WebElement editButtonInStudentInfo;
+
+
+    @FindBy(xpath = "(//*[@class='fw-semibold m-2 form-label'])[4]")
+    public WebElement nameTextInStudentInfo;
+
+
+
+    @FindBy(xpath = "//*[@id='lessonId']")
+    public WebElement lessonIdTextBox;
+
+    @FindBy(xpath = "//*[@id='educationTermId']")
+    public WebElement educationTermIdTextBox;
+
+    @FindBy(xpath = "(//*[@id='absentee'])[2]")
+    public WebElement absenteeTextBoxInStudentInfo;
+
+    @FindBy(xpath = "(//*[@id='midtermExam'])[2]")
+    public WebElement midtermExamTextBoxInStudentInfo;
+
+    @FindBy(xpath = "(//*[@id='finalExam'])[2]")
+    public WebElement finalExamTextBoxInStudentInfo;
+
+    @FindBy(xpath = "(//*[@id='infoNote'])[2]")
+    public WebElement infoNoteTextBoxInStudentInfo;
+
+    @FindBy(xpath = "(//*[@class='fw-semibold btn btn-primary btn-lg'])[2]")
+    public WebElement submitButtonInStudentınfo;
+
+
+
+
+
+
+
+
+
+}
