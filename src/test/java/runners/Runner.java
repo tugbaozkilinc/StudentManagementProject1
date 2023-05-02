9a9c937b9fb44e6335fc36f8f628a58ce861c488
Emin Z <meminzunluoglu@gmail.com>
2023-05-02 22:29:57 +0300

    .

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -17,7 +17,6 @@ import org.junit.runner.RunWith;
         monochrome = true,
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        tags = "@vice_dean_message",
         dryRun = false
 )
 public class Runner {
